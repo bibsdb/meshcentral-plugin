1da279285cec13bb7a9637164dc8b1284c3f119b
LimeSlime11 <bartoszbrozio@gmail.com>
2026-05-28 10:02:06 +0200

    try again

--- plugin.js
+++ plugin.js
@@ -9,7 +9,13 @@
 module.exports.plugin = function (parent) {
     var obj = {};
     obj.parent = parent;
-    obj.exports = ["onDesktopDisconnect"];
+    
+    // EXPLICIT FIX: MeshCentral requires frontend files to be exported 
+    // inside this array for the server to serve them to web clients.
+    obj.exports = [
+      "onDesktopDisconnect",
+      "plugin-ui.js" 
+    ];
     
     obj.onDesktopDisconnect = function() {  
         writeDeviceEvent(encodeURIComponent(currentNode._id));  
