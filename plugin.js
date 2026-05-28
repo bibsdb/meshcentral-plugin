3326f4896371553decbee42315e3702c485e7e3b
LimeSlime11 <bartoszbrozio@gmail.com>
2026-05-28 10:12:09 +0200

    add console messages for debgging

--- plugin.js
+++ plugin.js
@@ -6,24 +6,26 @@
 
 "use strict";
 
+// 1. THIS RUNS IMMEDIATELY WHEN THE SERVER STARTS AND DETECTS THE PLUGIN
+console.log("=================================================");
+console.log("[MeshCentral SERVER] plugin.js has been successfully executed by Node.js!");
+console.log("=================================================");
+
 module.exports.plugin = function (parent) {
     var obj = {};
     obj.parent = parent;
-    
-    // Clean this up! ONLY list actual backend functions here.
     obj.exports = ["onDesktopDisconnect"];
     
+    // 2. THIS RUNS EVERY TIME A USER REFRESHES/LOGS INTO THE WEB PAGE
+    obj.onWebUIStartupEnd = function(arr) {
+        console.log("[MeshCentral SERVER] onWebUIStartupEnd hook triggered! Injecting plugin-ui.js into user session.");
+        arr.push('meshcentral-plugins/plugin/plugin-ui.js');
+    };
+
     obj.onDesktopDisconnect = function() {  
         writeDeviceEvent(encodeURIComponent(currentNode._id));  
         Q('d2devEvent').value = Date().toLocaleString()+': '; 
         focusTextBox('d2devEvent');
-    };
-
-    // THIS IS THE CORRECT METHOD:
-    // This server hook intercepts the Web UI compilation and injects your script tag natively
-    obj.onWebUIStartupEnd = function(arr) {
-        // Tells MeshCentral to append your script to the main web header stream
-        arr.push('meshcentral-plugins/plugin/plugin-ui.js');
     };
     
     return obj;
