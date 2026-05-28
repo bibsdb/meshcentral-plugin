193de5a27b0eb06520d832b092483231e39a17e7
LimeSlime11 <bartoszbrozio@gmail.com>
2026-05-28 10:06:28 +0200

    try again

--- plugin.js
+++ plugin.js
@@ -10,17 +10,20 @@ module.exports.plugin = function (parent) {
     var obj = {};
     obj.parent = parent;
     
-    // EXPLICIT FIX: MeshCentral requires frontend files to be exported 
-    // inside this array for the server to serve them to web clients.
-    obj.exports = [
-      "onDesktopDisconnect",
-      "plugin-ui.js" 
-    ];
+    // Clean this up! ONLY list actual backend functions here.
+    obj.exports = ["onDesktopDisconnect"];
     
     obj.onDesktopDisconnect = function() {  
         writeDeviceEvent(encodeURIComponent(currentNode._id));  
         Q('d2devEvent').value = Date().toLocaleString()+': '; 
         focusTextBox('d2devEvent');
+    };
+
+    // THIS IS THE CORRECT METHOD:
+    // This server hook intercepts the Web UI compilation and injects your script tag natively
+    obj.onWebUIStartupEnd = function(arr) {
+        // Tells MeshCentral to append your script to the main web header stream
+        arr.push('meshcentral-plugins/plugin/plugin-ui.js');
     };
     
     return obj;
